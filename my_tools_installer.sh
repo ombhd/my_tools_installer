c0f10fdf79a8969e66263adaf25d426634cb5794
Omar Bouykourne <obouykou@e2r7p5.1337.ma>
2020-12-05 09:30:55 +0100

    install brew and valgrind in goinfre

---
+++ my_tools_installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Author Omar BOUYKOURNE
+#42login : obouykou
+
+echo -e "\n\033[33m------- Downloading brew ... -------\033[0m\n"
+curl -L https://github.com/Homebrew/brew/archive/1.9.0.tar.gz > brew1.9.0.tar.gz &>/dev/null
+echo -e "\n\033[33m------- Installing brew ... -------\033[0m\n"
+tar -xvzf brew1.9.0.tar.gz	&>/dev/null
+rm -rf brew1.9.0.tar.gz		&>/dev/null
+mv brew-1.9.0 .brew			&>/dev/null
+rm -rf ~/goinfre/.brew		&>/dev/null
+cp -Rf .brew ~/goinfre		&>/dev/null
+echo -e "\n\033[32m------- brew has been installed successfully -------\033[0m\n"
+echo -e "\n\033[33m------- Downloading valgrind ... -------\033[0m\n"
+brew install --HEAD ./valgrind.rb &>/dev/null
+echo -e "\n\033[32m------- valgrind has been installed successfully -------\033[0m\n"
+
