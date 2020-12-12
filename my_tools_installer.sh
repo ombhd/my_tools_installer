75c51d5f6cd1bc1f6b3f33f7fca66366ad6242bf
OMAR BOUYKOURNE <54411060+su-omb@users.noreply.github.com>
2020-12-12 10:04:01 +0100

    Update my_tools_installer.sh

--- my_tools_installer.sh
+++ my_tools_installer.sh
@@ -3,21 +3,21 @@
 #42login : obouykou
 
 echo -e "\n\033[33m------- Downloading brew ... -------\033[0m\n"
-curl -L https://github.com/Homebrew/brew/archive/1.9.0.tar.gz > brew1.9.0.tar.gz #&>/dev/null
+curl -L https://github.com/Homebrew/brew/archive/1.9.0.tar.gz > brew1.9.0.tar.gz 
 
 echo -e "\n\033[33m------- Installing brew ... -------\033[0m\n"
-tar -xvzf brew1.9.0.tar.gz	#&>/dev/null
-rm -rf brew1.9.0.tar.gz		#&>/dev/null
-mv brew-1.9.0 .brew			#&>/dev/null
-rm -rf ~/goinfre/.brew		#&>/dev/null
-cp -Rf .brew ~/goinfre		#&>/dev/null
+tar -xvzf brew1.9.0.tar.gz	
+rm -rf brew1.9.0.tar.gz	
+mv brew-1.9.0 .brew			
+rm -rf ~/goinfre/.brew		
+cp -Rf .brew ~/goinfre		
 echo -e "\n\033[32m------- brew has been installed successfully -------\033[0m\n"
 
 echo -e "\n\033[33m------- Downloading valgrind ... -------\033[0m\n"
-brew install --HEAD ./valgrind.rb #&>/dev/null
+brew install --HEAD ./valgrind.rb 
 echo -e "\n\033[32m------- valgrind has been installed successfully -------\033[0m\n"
 
 echo -e "\n\033[33m------- Installing htop ... -------\033[0m\n"
-brew install htop #&>/dev/null
+brew install htop 
 echo -e "\n\033[33m------- htop has been installed successfully -------\033[0m\n"
 
