9d90b8f153d57ca03f34ae680184b39b35acf28f
Omar Bouykourne <obouykou@e2r7p5.1337.ma>
2021-01-16 15:22:49 +0100

    make waht dmcm do a bit clair with some outputs

--- scripts/dmcm_installer.bash
+++ scripts/dmcm_installer.bash
@@ -21,8 +21,13 @@ fi
 
 if echo "alias dmcm='bash ~/docker_start_up.bash'" >> "$shell_f";
 then
-	echo -e "\n\033[32m -- dmcm command has been successfully installed ! Enjoy :) --\n\033[0m"
-	echo -e "\033[36m -- Please, run this command now : [\033[33m source $shell_f\033[0m\033[36m ] Then run [\033[33m dmcm name_of_machine \033[0m\033[36m]--\n\033[0m" 
+	echo -e "\n\033[32m -- dmcm command has been successfully installed! Enjoy :) --\n\n\033[0m"
+	echo -e "\033[36m ===> Please, run this command now : [\033[33m source $shell_f\033[0m\033[36m ] --\n\n\033[0m" 
+	echo -e "\033[36m -- You can use dmcm command this way: [\033[33m dmcm name_of_machine \033[0m\033[36m]--\n\033[0m" 
+	echo -e "\033[36m -- dmcm command is just an alias to:\n" 
+	echo -e " ◊ Some cleaning commands, related to docker-machine and docker!\n" 
+	echo -e " ◊ [\033[33m docker-machine create --driver virtualbox name_of_machine \033[0m\033[36m]\n\033[0m" 
+	echo -e "\033[36m -- It creates a virtual machine with the name you submit for docker --\n\033[0m" 
 else
 	echo -e "\033[31m\n -- dmcm command has NOT been installed ! :( --\n\033[0m"
 	exit 1
