eaca806e5f25066f6b9d87a229d483f53f623b95
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2021-11-30 00:40:49 +0100

    fix: don't show already installed programs

--- scripts/is_confirmed.bash
+++ scripts/is_confirmed.bash
@@ -5,12 +5,7 @@
 get_conf()
 {
  	while true; do
-		if [[ $2 == 0 ]]; then
-			echo -en "\n\033[33m$1 is not installed in goinfre\n\033[0m"
-			echo -en "\033[33mDo you want to install $1 in goinfre ? \033[0m"
-		else
-			echo -en "\n\033[33mDo you want to install $1 ? \033[0m"
-		fi
+		echo -en "\n\033[33m --- $1 ? \033[0m"
 		read -r confirmation
 		case $confirmation in
 			[Yy]* ) confirmation=1 && break;;
@@ -25,12 +20,7 @@ is_installed=$(which "$1" &>/dev/null; echo -n $?)
 path=$(which "$1")
 
 if [[ "$is_installed" == "0" && "$path" != *"goinfre"* ]]; then
-	get_conf "$1" 0
-	if [ "$confirmation" == "1" ]; then
-		echo -en "\n\033[33mPlease, remove your version of $1 (remove the whole directories of $1), then retry the installation\033[0m\n"
-		sleep 2
-		confirmation=0
-	fi
+	confirmation=0
 
 elif [ "$is_installed" == "1" ]; then
 	get_conf "$1" 1
